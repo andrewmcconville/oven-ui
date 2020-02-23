09d066d0f9b772df4985c04693986a3f2577a209
Andrew McConville <andrew@andrewmcconville.com>
2020-02-23 10:10:33 -0600

    workflow test 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>Andrew's workflow test. Edit, Save, Push from whereever. Pull, Build, Serve from raspberry pi.</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
